72e59f0fb8ac0a7f09ee103a217c16c471e43cff
reyhane79 <reyhane79@gmail.com>
2023-10-20 13:35:03 +0330

    add documentation

---
+++ frequency estimation for all windows/frequency_estimation.m
@@ -0,0 +1,40 @@
+[spch,Fs]=audioread('a.wav');
+fl=400; %**** sample number of each frame(N)
+fs=160; %**** frame shift(M)
+
+
+FN=ceil(length(spch)/fs);
+f_num=1:FN;
+y= buffer (spch,fl,fl-fs);%har frame be yek sotoon tabdil mishavad
+avr=zeros(1,floor(FN));
+w1=hamming(fl);
+%w2=rectwin(fl);
+for i=1:FN
+    y(:,i)=y(:,i).*w1;
+    y(:,i)=y(:,i).*w1; %windowing
+end
+%*******************************
+energy=zeros(1,size(y,2));
+for i=1:size(y,2)
+    frame=y(:,i);
+    frameEnergy=transpose(frame)*frame;
+    energy(1,i)=frameEnergy;
+end
+[maxEnergy,Indice] = max(energy);
+
+figure('Name','cepstral coeficients calculation result','NumberTitle','off')
+xlabel('samples')
+ylabel('frequency')
+title('a.wav pitch frequency based on cepstral analysis')
+xlim([1,size(y,2)]);
+ylim([1,400]);
+hold on;
+
+%pitchi= cepstrum(y(:,100),Fs);
+
+for i=1:size(y,2)
+    if(energy(1,i)>0.333*maxEnergy)
+        pitchi= cepstrum(y(:,i),Fs);
+        plot(i,pitchi,'rx');
+    end
+end
---
+++ frequency estimation for one window/cepstrum.m
@@ -0,0 +1,29 @@
+
+function pitchi = cepstrum(x, fs)
+
+% x - signal in the time domain
+% fs - sampling frequency, Hz
+% C - real cepstrum
+
+
+% length of the signal
+N = length(x);
+
+
+% calculate the number of unique fft points
+NumUniquePts = ceil((N+1)/2);
+z=(1:NumUniquePts);
+% cepstral analysis
+C = real(ifft(log(abs(fft(x)))));       % cepstrum
+C = C(1:NumUniquePts);                  % because of simmerty of cepstrum function
+
+
+%for pitch frequency
+highFilter=[zeros(20,1);ones(size(C,1)-20,1)];
+pitchQfrency=highFilter.*C;
+[~,IPos] = max(pitchQfrency);
+pitchi=fs/IPos;
+plot(z,pitchQfrency);
+xlabel('quefrency(samples)')
+ylabel('amplitude')
+end
